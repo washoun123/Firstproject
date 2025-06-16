4ac84980f9268ab179dcc44c197e132a9533d812
washoun123 <zomrawi495@gmail.com>
2025-06-16 17:24:33 +0200

    test

--- doolight/src/App.js
+++ doolight/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
